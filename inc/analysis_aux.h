69f3e376ea657e84345c90ce9b0b33831aeeca69
moisesmsa <moises78zito@hotmail.com>
2019-03-16 20:32:50 +0000

    save_avg fixed bug check if avg time is empty

--- inc/analysis_aux.h
+++ inc/analysis_aux.h
@@ -8,9 +8,9 @@ void run_search(std::function <itr (int, itr, itr) > search, int, int);
 void fill_data(int);
 void print_time();
 
-void save_time(double);
-void save_dataset_interval(int);
-
+void save_avg(double);
+void save_input_size(int);
+void save_labels(char);
 // void saveTime(double, std::vector<double>);
 
 // void writeData(std::vector<double> &);
--- src/analysis_aux.cpp
+++ src/analysis_aux.cpp
@@ -1,14 +1,13 @@
 #include "../inc/analysis_aux.h"
 
 std::vector<long int> dataset;
-std::vector<double> time_data;
-std::vector<int> dataset_interval;
-std::vector<char> data_label;
+std::vector<double> time_avg;
+std::vector<int> input_size;
+std::vector<char> searchs_labels;
 
 void fill_data(int data_size){
 	std::cout << "alocatting data vector ..." << std::endl;
 	for (int i = 0; i < data_size; ++i){
-		// std::cout << i << std::endl;
 		dataset.push_back(i); 
 	} 
 	std::cout << "allocation finished!" << std::endl;
@@ -16,25 +15,25 @@ void fill_data(int data_size){
 
 void run_search(std::function <itr (int ,itr, itr)> search, int key, int sampling){
 	
-	auto first = dataset.begin();
-	auto last = first + sampling;
-		
+	auto first = dataset.begin(), last = first + sampling;
+	std::chrono::duration<double> avg_exec;
+
 	while(last <= dataset.end())
 	{
 		std::cout << "running search" << std::endl;
-		save_dataset_interval(std::distance(first, last));
+		save_input_size(std::distance(first, last));
 
-		auto init = std::chrono::high_resolution_clock::now();
-		search(key, first, last);
-		auto end = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < 100; ++i)
+		{
+			auto init = std::chrono::high_resolution_clock::now();
+			search(key, first, last);
+			auto end = std::chrono::high_resolution_clock::now();
 
-	    std::chrono::duration<double> interval = end - init; 
-
-	    save_time(interval.count());
-	    std::cout << interval.count() << "ms" << std::endl;
+	 	   	avg_exec += (end - init);
+		}
+	    save_avg(avg_exec.count());
 	
 	    last += sampling;
-	    std::cout << *last << std::endl;
 	}
 }
 
@@ -43,22 +42,27 @@ int calc_sampling(int sampling, int data_size){
 	return sampling;
 }
 
-void print_time(){
-	std::cout << "time vector size: " << time_data.size() << std::endl;
-	std::cout << "data vector size: " << dataset.size() << std::endl;
-	for (auto i = time_data.begin(); i < time_data.end(); ++i) 
-	{
-		std::cout << *i << std::endl;
+
+void save_avg(double t_interval){
+	if(time_avg.empty()){
+		std::cout << "empty avg time!" << std::endl;
+		time_avg.push_back(t_interval/100);
+	}else{
+		
+		auto last_avg = time_avg.end();
+		t_interval = *last_avg + (t_interval - *last_avg)/100;
+		
+		time_avg.push_back(t_interval);
 	}
 }
 
-void save_time(double t_interval){
-	time_data.push_back(t_interval);
-}
-
-void save_dataset_interval(int d_interval){
-	dataset_interval.push_back(d_interval);
+void save_input_size(int d_interval){
+	input_size.push_back(d_interval);
 }	
+
+void save_labels(char label){
+	searchs_labels.push_back(label);
+}
 
 // void write_data(std::vector<double> &time){
 // 	ofstream grap("analyze graph");
@@ -75,3 +79,10 @@ void save_dataset_interval(int d_interval){
 // double average(){
 
 // }
+
+void print_time(){
+	std::cout << "time vector size: " << time_avg.size() << std::endl;
+	std::cout << "data vector size: " << dataset.size() << std::endl;
+	for (auto i = time_avg.begin(); i < time_avg.end(); ++i) 
+		std::cout << *i << std::endl;
+}
