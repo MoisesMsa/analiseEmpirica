6b863c0c4e52b34355cacf563025eea1b0be26d2
moisesmsa <moises78zito@hotmail.com>
2019-03-16 01:03:52 +0000

    data global vars, run_search, print time

--- src/search.cpp
+++ src/search.cpp
@@ -1,5 +1,8 @@
 #include "search.h"
 
+std::vector<double> run_time;
+std::vector<long int> data;
+
 
 itr linear(int key, itr l, itr r)
 {
@@ -335,8 +338,39 @@ itr ternary_r(int key, itr l, itr r){
 }
 
 
+void fill_data(int data_size){
+	std::cout << "alocatting data vector ..." << std::endl;
+	for (int i = 0; i < data_size; ++i){
+		std::cout << i << std::endl;
+		data.push_back(i); 
+	} 
+	std::cout << "allocation finished!" << std::endl;
+}
+
+void run_search(std::function <itr (int ,itr, itr)> search, int key, int data_total){
+	// while(functions != end){
+
+	auto init = std::chrono::system_clock::now();
+	search(key, data.begin(), data.end());
+	auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> interval = end - init; 
+    // std::cout << interval.count() << "ms" << std::endl;
+    run_time.push_back(interval.count());
+	// ++functions;
+	//aumentar intervalo no vector
+	// }
+}
 
 
+void print_time(){
+	std::cout << "time vector size: " << run_time.size() << std::endl;
+	std::cout << "data vector size: " << data.size() << std::endl;
+	for (auto i = run_time.begin(); i < run_time.end(); ++i) 
+	{
+		std::cout << *i << std::endl;
+	}
+}
 
 
 // void saveTime(int interval, std::vector){
--- src/search.h
+++ src/search.h
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <chrono>
 
 using itr = std::vector<long int>::iterator;
+
 
 
 itr binary(int, itr, itr);
@@ -9,11 +12,16 @@ itr fibonacci(int, itr, itr);
 itr ternary(int key, itr l, itr r);
 itr jump(int key, itr l, itr r);
 itr linear(int key, itr l, itr r);
-
 itr binary_r(int key, itr l, itr r);
 itr ternary_r(int key, itr l, itr r);
 
 int generateFib(itr , itr);
+
+void run_search(std::function <itr (int, itr, itr) > search, int ,int );
+
+void fill_data(int);
+void print_time();
+
 // void saveTime(double, std::vector<double>);
 
 // void writeData(std::vector<double> &);
