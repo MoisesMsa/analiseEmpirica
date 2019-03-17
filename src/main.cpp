d29c465c63933b0c05f2508f39092aca38a8b0c7
moisesmsa <moises78zito@hotmail.com>
2019-03-17 01:14:55 +0000

    read and validate command line

--- src/main.cpp
+++ src/main.cpp
@@ -8,51 +8,59 @@ int main(int argc, char const *argv[])
 
 	/*!!! NUMERO DE AMOSTRAS É DIFERENTE DO TAMANHO DO VETOR !!!!*/
 
+
+
 	int data_size = 100;
 	int sampling = 20;
-
-	std::vector<std::function<itr(int,itr,itr)> > searchs_v = {linear, binary, fibonacci, ternary, jump, binary_r, ternary_r};
-
-	
-	control_flux(searchs_v, -10, data_size, sampling);
+	std::vector<std::function<itr(int,itr,itr)> > searchs_v;
 	
 	// sampling = calc_sampling(sampling, data_size);
 
-	// if(argc == 1){
-		
-	// 	std::cout << "init" << std::endl;
-	// 	auto search = func_v.begin();
-	// 	fill_data(data_size);
-	// 	run_search(*search, -10, sampling);
+	if(argc == 1){
 
-	// }else if(argc > 1 && argc < 7){
+		control_flux(searchs_v, -10, data_size, sampling);
+		searchs_v = {linear, binary, fibonacci, ternary, jump, binary_r, ternary_r};
+	
+	}
+	else if(argc > 1 && argc < 9)
+	{
+		for (int i = 0; i < argc; ++i)
+		{
+			if(std::string(argv[i]) == "lin")
+			{
+				searchs_v.push_back(linear);
+			}
+			else if(std::string(argv[i]) == "bin")
+			{
+				searchs_v.push_back(binary);
+			}
+			else if(std::string(argv[i]) == "fib")
+			{
+				searchs_v.push_back(fibonacci);
+			}
+			else if(std::string(argv[i]) == "ter")
+			{
+				searchs_v.push_back(ternary);
+			}else if(std::string(argv[i]) == "jum"){
+				searchs_v.push_back(jump);
+			}else if(std::string(argv[i]) == "bin_r"){
+				searchs_v.push_back(binary_r);
+			}else if(std::string(argv[i]) == "ter_r"){
+				searchs_v.push_back(ternary_r);
+			}
+		}
 		
-	// 	control_flux(func_v, sampling);
-	// }
-	// // else if(argc > 2){
-	// 	// for (int i = 0; i < argc; ++i)
-	// 	// {
-	// 	// 	if(std::string(argv[i]) == "linear")
-	// 	// 	{
-	// 	// 		std::cout << "busca linear: " << std::endl;
-	// 	// 	}else if(std::string(argv[i]) == "binary")
-	// 	// }
+		control_flux(searchs_v, -10, data_size, sampling);
 
-	// 	// std::cout << std::endl;
-	// // }
-	// else if(argc > 7){
-	// 	std::cout << "type a valid entry!" << std::endl;
-	// 	return -1;
-	// }
+	}
+	else if(argc > 9){
+		std::cout << "type a valid entry!" << std::endl;
+		return -1;
+	}
 
 	
 	// std::function<itr (int, itr, itr) > search = binary;
 	
 	
-	// #ifdef DEBUG
-	print_time();
-	// #endif
-
-
 	return 0;
 }
